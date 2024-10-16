4f804ec466140032772f51bce7563b2a8b5d77cd
Anh Do <anhdongoc18@gmail.com>
2024-10-16 23:57:48 +1030

    rabbit

--- Ecommerce.MessageBus/MessageBus.cs
+++ Ecommerce.MessageBus/MessageBus.cs
@@ -11,21 +11,21 @@ namespace Ecommerce.MessageBus
     public class MessageBus : IMessageBus
     {
 
-        // public async Task PublishMessage(object message, string topic_queue_Name)
-        // {
-        //     await using var client = new ServiceBusClient(AzureKey.KEY_AZURE);
+        public async Task PublishMessage(object message, string topic_queue_Name)
+        {
+            await using var client = new ServiceBusClient(AzureKey.KEY_AZURE);
 
-        //     ServiceBusSender sender = client.CreateSender(topic_queue_Name);
+            ServiceBusSender sender = client.CreateSender(topic_queue_Name);
 
-        //     var jsonMessage = JsonConvert.SerializeObject(message);
-        //     ServiceBusMessage finalMessage = new ServiceBusMessage(Encoding
-        //         .UTF8.GetBytes(jsonMessage))
-        //     {
-        //         CorrelationId = Guid.NewGuid().ToString(),
-        //     };
+            var jsonMessage = JsonConvert.SerializeObject(message);
+            ServiceBusMessage finalMessage = new ServiceBusMessage(Encoding
+                .UTF8.GetBytes(jsonMessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString(),
+            };
 
-        //     await sender.SendMessageAsync(finalMessage);
-        //     await client.DisposeAsync();
-        // }
+            await sender.SendMessageAsync(finalMessage);
+            await client.DisposeAsync();
+        }
     }
 }
